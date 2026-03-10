56c3cf1975f3c83a0cd76f2a20d549261f93b11f
Quynhnhu2003 <nhu24icloud@gmail.com>
2026-03-10 17:36:36 +0700

    fix bugs

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-import Booking from "@/components/Booking";
-import Footer from "@/components/Footer";
-import Navbar from "@/components/Navbar";
-import ServicesSection from "@/components/ServicesSection";
-import TestimonialsSection from "@/components/TestimonialsSection";
+import Booking from "../components/Booking";
+import Footer from "../components/Footer";
+import ServicesSection from "../components/ServicesSection";
+import TestimonialsSection from "../components/TestimonialsSection";
+import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
