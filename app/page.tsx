e3c88925e716abc3eedc813eef07fe95742b48fe
Quynhnhu2003 <nhu24icloud@gmail.com>
2026-03-10 18:01:14 +0700

    fix import path for Navbar and Footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-import Booking from "../components/Booking/Booking";
-import Footer from "../components/Footer/Footer";
-import Navbar from "../components/Navbar/Navbar";
-import ServicesSection from "../components/ServicesSection/ServicesSection";
-import TestimonialsSection from "../components/TestimonialsSection/TestimonialsSection";
+import Booking from "@/components/Booking/Booking";
+import Footer from "@/components/Footer/Footer";
+import Navbar from "@/components/Navbar/Navbar";
+import ServicesSection from "@/components/ServicesSection/ServicesSection";
+import TestimonialsSection from "@/components/TestimonialsSection/TestimonialsSection";
 
 export default function Home() {
   return (
